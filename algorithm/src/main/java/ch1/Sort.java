c10576c4d5127f326cfefe90d875eb2225c2d4a7
yu.chao@starrides.com <yu.chao@starride.cn>
2021-08-08 10:02:24 +0800

    Initial commit

---
+++ algorithm/src/main/java/ch1/Sort.java
@@ -0,0 +1,21 @@
+package ch1;
+
+/**
+ * @author yuchao
+ * @create 2021-08-08-9:34 上午
+ */
+public class Sort {
+
+    public static void selectionSort() {
+
+    }
+
+
+    public static void bubbleSort() {
+
+    }
+
+    public static void insertSort() {
+
+    }
+}
