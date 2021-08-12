2880169519cd917a1f74f24081b53b4244c465bb
yu.chao@starrides.com <yu.chao@starride.cn>
2021-08-12 15:29:18 +0800

    k m times

--- algorithm/src/main/java/ch2/Code02_KMTimes.java
+++ algorithm/src/main/java/ch2/Code02_KMTimes.java
@@ -1,10 +1,34 @@
 package ch2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author yuchao
  * @create 2021-08-12-12:51 下午
  */
 public class Code02_KMTimes {
+
+    public static int test(int[] arr, int k, int m) {
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int num : arr) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        for (int num : map.keySet()) {
+            if (map.get(num) == k) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
 
     public static int onlyKTimes(int[] arr, int k, int m) {
 
@@ -32,6 +56,8 @@ public class Code02_KMTimes {
         int[] arr = new int[]{2, 2, 1, 1, 1, 1};
 
         System.out.println(onlyKTimes(arr, 2, 4));
+
+        System.out.println(test(arr, 2, 4));
     }
 
 }
