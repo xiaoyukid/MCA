701ccf23fadb5779be1dfb3bcda3718020f1815e
yu.chao@starrides.com <yu.chao@starride.cn>
2022-02-06 23:17:44 +0800

    heap sort

--- algorithm/src/main/java/ch4/Code02_HeapSort.java
+++ algorithm/src/main/java/ch4/Code02_HeapSort.java
@@ -18,8 +18,28 @@ public class Code02_HeapSort {
             heapInsert(arr, i);
         }
 
-        // TODO: 2022/2/6
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
 
+    }
+
+    //heapify 往下走，存在左孩子，且左孩子大于父节点
+    private static void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;
+        while (left < heapSize) {
+            int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? index * 2 + 2 : left;
+            largest = arr[largest] > arr[index] ? largest : index;
+            if (largest == index) {
+                break;
+            }
+            swap(arr, index, largest);
+            index = largest;
+            left = 2 * index + 1;
+        }
     }
 
     //heap insert 插入堆中 放入最后的位置 能否往上走
@@ -34,6 +54,32 @@ public class Code02_HeapSort {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray(20, 100);
+        printArray(arr);
+        heapSort(arr);
+        printArray(arr);
     }
 
 }
